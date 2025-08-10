a8d3c5dba5d418dd206c1926ced2156d5686624a
BahaaJber1 <bahaajber23@gmail.com>
2025-08-10 21:09:50 +0300

    Added disabled feature

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -51,6 +51,7 @@ function Filter({ filterField, options }) {
 					key={option.value}
 					onClick={() => handleclick(option.value)}
 					active={option.value === currentFilter}
+					disabled={option.value === currentFilter}
 				>
 					{option.label}
 				</FilterButton>
