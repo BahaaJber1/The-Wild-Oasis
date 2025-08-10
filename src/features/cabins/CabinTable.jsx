e7e0db54b323842246db6f3c673b9a82ad099703
BahaaJber1 <bahaajber23@gmail.com>
2025-08-10 21:09:14 +0300

    Added the sort logic and changed the default data to the sorted one

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,3 @@
-
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
@@ -7,14 +6,13 @@ import { useCabins } from "./useCabins";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
 
-
-
 function CabinTable() {
 	const { isLoading, cabins } = useCabins();
 	const [searchParams] = useSearchParams();
 
 	if (isLoading) return <Spinner />;
 
+	//1. Filter
 	const filterValue = searchParams.get("discount") || "all";
 
 	let filteredCabins;
@@ -23,6 +21,14 @@ function CabinTable() {
 		filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
 	if (filterValue === "with-discount")
 		filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+	//2. Sort
+	const sortBy = searchParams.get("sortBy") || "startDate-asc";
+	const [field, direction] = sortBy.split("-");
+	const modifier = direction === "asc" ? 1 : -1;
+	const sortedCabins = filteredCabins.sort(
+		(a, b) => (a[field] - b[field]) * modifier
+	);
 
 	return (
 		<Menus>
@@ -37,7 +43,7 @@ function CabinTable() {
 				</Table.Header>
 
 				<Table.Body
-					data={filteredCabins}
+					data={sortedCabins}
 					render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
 				/>
 			</Table>
