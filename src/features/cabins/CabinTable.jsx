c78c11a5c4237c4072b56612f33f4c9dc2e6525a
BahaaJber1 <bahaajber23@gmail.com>
2025-08-10 22:05:33 +0300

    Added another if case to handle the empty cabins data

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,12 +5,15 @@ import Table from "../../ui/Table";
 import { useCabins } from "./useCabins";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
 	const { isLoading, cabins } = useCabins();
 	const [searchParams] = useSearchParams();
 
 	if (isLoading) return <Spinner />;
+
+	if (!cabins.length) return <Empty resource="cabins" />;
 
 	//1. Filter
 	const filterValue = searchParams.get("discount") || "all";
