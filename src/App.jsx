0a48b4c7e707502b37cf9413ccf7a5412097d838
BahaaJber1 <bahaajber23@gmail.com>
2025-08-11 14:39:47 +0300

    Added new route to the bookings/:bookingId

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { Toaster } from "react-hot-toast";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // for dev tool kit.
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Booking from "./pages/Booking";
 const queryclient = new QueryClient({
 	// create a new object  from he QueryClient provided by react-query and pass an object for options
 	defaultOption: {
@@ -34,6 +35,7 @@ function App() {
 						<Route index element={<Navigate replace to="dashboard" />} />
 						<Route path="dashboard" element={<Dashboard />} />
 						<Route path="bookings" element={<Bookings />} />
+						<Route path="bookings/:bookingId" element={<Booking />} />
 						<Route path="cabins" element={<Cabins />} />
 						<Route path="users" element={<Users />} />
 						<Route path="settings" element={<Settings />} />
