f1f6a69e48ad143347e034901f2a75c9c1c30552
BahaaJber1 <bahaajber23@gmail.com>
2025-08-11 16:02:06 +0300

    Added the checkin/:bookingId router to the routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { Toaster } from "react-hot-toast";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // for dev tool kit.
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Booking from "./pages/Booking";
+import Checkin from "./pages/Checkin";
 const queryclient = new QueryClient({
 	// create a new object  from he QueryClient provided by react-query and pass an object for options
 	defaultOption: {
@@ -36,6 +37,7 @@ function App() {
 						<Route path="dashboard" element={<Dashboard />} />
 						<Route path="bookings" element={<Bookings />} />
 						<Route path="bookings/:bookingId" element={<Booking />} />
+						<Route path="checkin/:bookingId" element={<Checkin />} />
 						<Route path="cabins" element={<Cabins />} />
 						<Route path="users" element={<Users />} />
 						<Route path="settings" element={<Settings />} />
