9d5ff152031d586ffc5e3e8170bd7a3949af0b86
BahaaJber1 <bahaajber23@gmail.com>
2025-08-11 14:41:57 +0300

    Added custom hook to get the booking by id from the URL

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+    const {bookingId} = useParams();
+
+    // custom hook from react query we gonna provide object with 2 things
+    // 1.querykey = will uniqeuly Identify the query needs to be an array
+    // 2. the function that's gonna fetch the data from the api and needs to return a promise
+
+    const {
+        isLoading,
+        data: booking,
+        error,
+    } = useQuery({
+        queryKey: ["booking"],
+        queryFn: () => getBooking(bookingId),
+        retry: false,
+    });
+
+    return { isLoading, booking, error };
+}
