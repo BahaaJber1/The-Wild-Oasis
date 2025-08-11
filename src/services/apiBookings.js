1a4ee7e74d5f633b9ae03a32323b8378608e20d9
BahaaJber1 <bahaajber23@gmail.com>
2025-08-11 03:21:42 +0300

    Got the count from supabase query, added the paginiation logic

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,10 +1,13 @@
+import { PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
-    .select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)");
+    .select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
+      { count: "exact" } // tells supabase to return the count from this query too.
+    );
 
   // 1. Filter
   if (filter) query = query.eq(filter.field, filter.value)
@@ -12,7 +15,15 @@ export async function getBookings({ filter, sortBy }) {
   // 2. Sort
   if (sortBy) query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
-  const { data, error } = await query;
+  // 3. Pagination
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE - 1;
+
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
 
   // you can use these for hard coade comparision but this is not efficient
   // .eq("status", "unconfirmed")
@@ -23,7 +34,7 @@ export async function getBookings({ filter, sortBy }) {
     throw new Error("Bookings could not be loaded");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
