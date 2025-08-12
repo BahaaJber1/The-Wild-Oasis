e2c695e74f3dad478b3d9e4a021872c59a0932fb
BahaaJber1 <bahaajber23@gmail.com>
2025-08-12 21:46:51 +0300

    Used the hooks inside the page to check the data we get from react query

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,30 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+	const { bookings, isPending } = useRecentBookings();
+	const { stays, confirmedStays, isPending: isPending1 } = useRecentStays();
+
+	if (isPending || isPending1) return <Spinner />;
+
+
+	return (
+		<StyledDashboardLayout>
+			<div>Statistics</div>
+			<div>Today's activity</div>
+			<div>Chart stay durations</div>
+			<div>Chart sales</div>
+		</StyledDashboardLayout>
+	);
+}
+
+export default DashboardLayout;
