1d10191a5769583157db8cba51ea14bab0c84ab5
BahaaJber1 <bahaajber23@gmail.com>
2025-08-13 19:15:53 +0300

    Removed the credentials from the form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -9,8 +9,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-	const [email, setEmail] = useState("bahaa@test.com");
-	const [password, setPassword] = useState("0787584911");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 	const { login, isPending } = useLogin();
 
 	function handleSubmit(e) {
